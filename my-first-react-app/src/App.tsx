c39f5bb8363aade407576120b43ee4c35194eb28
ferxxogeek <ferjpp20@gmail.com>
2024-12-29 16:47:30 -0400

    segundo commit

--- my-first-react-app/src/App.tsx
+++ my-first-react-app/src/App.tsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
+
+  const persona = {
+    nombre: "Twilla",
+    edad: 22
+  }
+
+  const personas = [
+    { nombre: "Pedro", edad: 20 },
+    { nombre: "Fernando", edad: 22 },
+    { nombre: "Alejandro", edad: 23 },
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className='p-10'>
+      <h1 className='text-5xl font-bold underline text-blue-800 w-fit'>Hola mundo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className='bg-green-700 text-white px-4 py-2 font-bold rounded-full mt-5 hover:text-gray-100 hover:scale-110 transition-all'>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        {/* Esta es la impresión de un objeto */}
+        <p>{persona.nombre}: {persona.edad}</p>
+
+        {/* Esta es la impresión de un array  */}
+        {personas.map((fulano, index) => <p key={index}>Hola, {fulano.nombre}, tienes {fulano.edad}</p>)}
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
