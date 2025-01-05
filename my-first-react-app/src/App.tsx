c3b7cf93f3c932d5025639d619e810f961f5173f
ferxxogeek <ferjpp20@gmail.com>
2025-01-04 22:52:20 -0400

    commit dia 3

--- my-first-react-app/src/App.tsx
+++ my-first-react-app/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import CustomButton from './components/CustomButton'
-
 function App() {
-  const [count, setCount] = useState<number>(0)
-
-  const persona = {
-    nombre: "Twilla",
-    edad: 22
-  }
-
-  const personas = [
-    { nombre: "Pedro", edad: 20 },
-    { nombre: "Fernando", edad: 22 },
-    { nombre: "Alejandro", edad: 23 },
-  ]
-
   return (
-    <div className='p-10'>
-      <h1 className='text-5xl font-bold underline text-orange-600 w-fit'>Hola mundo</h1>
-        {/*Este es un botón con estilos condicionales */}
-        <button className={`${count > 10 ? 'bg-purple-700' : 'bg-blue-700'} text-white px-4 py-2 font-bold rounded-full mt-5 hover:text-gray-100 hover:scale-110 transition-all`} onClick={() => setCount(count + 1)} >
-          count is {count}
-        </button>
+    <div className="h-screen flex flex-col justify-center space-y-10 bg-blue-400 p-10">
+    
+      <div className="flex flex-col sm:flex-row space-y-20 sm:space-y-0 sm:space-x-20 justify-center">
+        <div className="w-10 h-10 rounded-full bg-red-500 border border-black"/>
+        <div className="w-10 h-10 rounded-full bg-green-500 border border-black"/>
+        <div className="w-10 h-10 rounded-full bg-yellow-500 border border-black"/>
+      </div>
+      
+      <div className="h-20 grid grid-cols-12">
+        <div className="w-full h-full bg-red-500 col-span-6"/>
+        <div className="w-full h-full bg-green-500 col-span-6"/>
+        <div className="w-full h-full bg-yellow-500 col-span-6"/>
+      </div>
 
-        {/*Esto es una condición if-else */}
-        {count > 10
-          ? <p>{persona.nombre}</p>
-          : personas.map((fulano, index) => <p key={index}>{fulano.nombre}</p>)
-        }
-
-        <CustomButton text={`resta el contador ${count}`} bgColor='blue-800' cb={() => setCount(count - 1)} />
     </div>
   )
 }
